8825a83ac86853591efdb293e1776f2b8827d5a0
Shreyash Soni <yashsoni96@gmail.com>
2024-07-18 19:57:44 +0530

    add code for dot product

---
+++ 001_math/002_math_dotproduct.py
@@ -0,0 +1,37 @@
+# %% [markdown]
+# ### dot product!
+
+# %%
+# import libraries
+import numpy as np
+import torch
+
+# %% [markdown]
+# # Using numpy
+
+# %%
+# create a vector
+nv1 = np.array([1,2,3,4])
+nv2 = np.array([0,1,0,-1])
+
+# dot product via function
+print(np.dot(nv1,nv2))
+
+# dot product via computation
+print(np.sum( nv1*nv2 ))
+
+# %% [markdown]
+# # Using pytorch
+
+# %%
+# create a vector
+tv1 = torch.tensor([1,2,3,4])
+tv2 = torch.tensor([0,1,0,-1])
+
+# dot product via function
+print(torch.dot(tv1,tv2))
+
+# dot product via computation
+print(torch.sum( tv1*tv2 ))
+
+
